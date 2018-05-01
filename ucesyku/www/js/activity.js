371326c23d80a085c2000f04827e094854474cd5
yilingkuo <lyn3023298@gmail.com>
2018-05-01 13:29:51 -0400

    attempt to activate touchscreen trigger by touchStart

--- ucesyku/www/js/activity.js
+++ ucesyku/www/js/activity.js
@@ -9,16 +9,49 @@ attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a
 id: 'mapbox.streets'
 }).addTo(mymap);
 
-
-function trackLocation(){
-mymap.locate({setView: true,maxZoom: 20, watch: true}) /* This will return map so you can do chaining */
-        .on('locationfound', function(e){
-            var marker = L.marker([e.latitude, e.longitude]).addTo(mymap).bindPopup('Your are here :)');
-            var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+// Found location and add marker with accuracy of position
+function onLocationFound(){
+var marker;
+var circle;	
+mymap.locate({enableHighAccuracy:true, setView: true,maxZoom: 20, watch: true}) /* This will return map so you can do chaining */
+        .on('touchstart', function(e){
+			var lat = (e.latitude);
+			var lng = (e.longitude);
+			var newLatLng = new L.LatLng(lat, lng);
+			if (typeof(marker)==='undefined'){
+				marker= new L.marker([e.latitude, e.longitude]);
+				marker.addTo(mymap).bindPopup('Your are here :)');
+				circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
                 weight: 1,
                 color: 'blue',
                 fillColor: '#cacaca',
                 fillOpacity: 0.2
             }).addTo(mymap);
-			mymap.fitBounds(circle)
+			}
+			else{
+				marker.setLatLng(newLatLng);
+				circle.setLatLng(newLatLng);
+			}
+			//var marker = L.marker([e.latitude, e.longitude]).addTo(mymap);
+            //marker = L.marker([e.latitude, e.longitude]).update(marker).bindPopup('Your are here :)');
+            //var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+            //    weight: 1,
+            //    color: 'blue',
+            //    fillColor: '#cacaca',
+            //    fillOpacity: 0.2
+            //}).addTo(mymap);
+			//marker.setLatLng(newLatLng); 
+			//circle.setLatLng(newLatLng)
+			mymap.fitBounds(circle);
 })};
+
+function getCoords(){
+	mymap.on('click touchstart', function(e) {
+		
+		var marker_coord = L.marker([e.latlng.lat, e.latlng.lng],{icon:testMarkerGreen}).addTo(mymap).bindPopup('<br>Point coordinate at </br><br><b>Lon: ' + e.latlng.lng + "</b></br><br><b>Lat: " + e.latlng.lat+'</br>');
+})};
+
+var testMarkerGreen=L.AwesomeMarkers.icon({
+
+markerColor:'green'
+});

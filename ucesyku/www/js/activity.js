8f520cca0367ff09d8d76647758e3ac62bd17d40
yilingkuo <lyn3023298@gmail.com>
2018-05-06 11:05:41 -0400

    adjust the getCoord function in activity.js. Fill the coords to the form automatically once the marker is added, and remove previous marker if a new marker is added

--- ucesyku/www/js/activity.js
+++ ucesyku/www/js/activity.js
@@ -45,12 +45,15 @@ mymap.locate({enableHighAccuracy:true, setView: true,maxZoom: 20, watch: true}) 
 			mymap.fitBounds(circle);
 })};
 
+var marker_coord;
 function getCoords(){
 	mymap.on('click touchstart', function(e) {
+		if (marker_coord != null)mymap.removeLayer(marker_coord);
+		marker_coord = L.marker([e.latlng.lat, e.latlng.lng],{icon:testMarkerGreen}).addTo(mymap).bindPopup('<br>Point coordinate at </br><br><b>Lon: ' + e.latlng.lng + "</b></br><br><b>Lat: " + e.latlng.lat+'</br>');
+		document.getElementById('longitude').value =  e.latlng.lng;
+		document.getElementById('latitude').value =  e.latlng.lat;
 		
-		var marker_coord = L.marker([e.latlng.lat, e.latlng.lng],{icon:testMarkerGreen}).addTo(mymap).bindPopup('<br>Point coordinate at </br><br><b>Lon: ' + e.latlng.lng + "</b></br><br><b>Lat: " + e.latlng.lat+'</br>');
 })};
-
 var testMarkerGreen=L.AwesomeMarkers.icon({
 
 markerColor:'green'
